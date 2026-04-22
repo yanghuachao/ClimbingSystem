d56f3b7604949462ef578f5bd917ef7e59169b25
yanghuachao <1659641771@qq.com>
2026-04-22 10:40:01 +0800

    ClimbSystem

---
+++ Source/ClimbingSystem/Private/Component/CustomMovementComponent.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component/CustomMovementComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+#pragma region ClimbTraces
+
+TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape)
+{
+	TArray<FHitResult> OutCapsuleTraceHitResult;
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,
+		Start,
+		End,
+		ClimbCapsuleTraceRadius,
+		ClimbCapsuleTarceHalfHeight,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutCapsuleTraceHitResult,
+		false
+	);
+
+	return OutCapsuleTraceHitResult;
+}
+
+#pragma endregion
---
+++ Source/ClimbingSystem/Public/Component/CustomMovementComponent.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CustomMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIMBINGSYSTEM_API UCustomMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+private:
+#pragma region ClimbTraces
+
+	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false);
+#pragma endregion
+
+#pragma region ClimbVariables
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement:Climbing", meta = (AllowPrivateAccess = "true"))
+	TArray<TEnumAsByte<EObjectTypeQuery> > ClimbableSurfaceTraceTypes;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement:Climbing", meta = (AllowPrivateAccess="true"))
+	float ClimbCapsuleTraceRadius = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement:Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTarceHalfHeight = 72.f;
+#pragma endregion
+};
